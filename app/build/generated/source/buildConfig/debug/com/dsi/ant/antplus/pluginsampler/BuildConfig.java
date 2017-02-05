9585c054a69f32d1bd244007a3567004ad816a8d
Young Cho <ycho1213@gmail.com>
2017-02-04 21:54:48 -0600

    Initial commit

---
+++ app/build/generated/source/buildConfig/debug/com/dsi/ant/antplus/pluginsampler/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.dsi.ant.antplus.pluginsampler;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.dsi.ant.antplus.pluginsampler";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 30500;
+  public static final String VERSION_NAME = "3.5.0";
+}
